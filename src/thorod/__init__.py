565a81e9c9e033c2138086e54c33fa82c723f5fa
thebigmunch <mail@thebigmunch.me>
2019-02-23 07:46:02 -0500

    Code style

--- src/thorod/__init__.py
+++ src/thorod/__init__.py
@@ -1,11 +1,7 @@
 """A set of useful torrent scripts."""
 
-from .__about__ import (
-	__author__, __author_email__, __copyright__, __license__,
-	__summary__, __title__, __url__, __version__, __version_info__
-)
+from .__about__ import *
 
 __all__ = [
-	'__author__', '__author_email__', '__copyright__', '__license__',
-	'__summary__', '__title__', '__url__', '__version__', '__version_info__'
+	*__about__.__all__,
 ]
