35a1537ff8f7653d2649b5a274d4ca5806097b4e
thebigmunch <mail@thebigmunch.me>
2020-01-11 10:50:16 -0500

    Use sphinx-material theme

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,8 @@
 import os
 import sys
 
+import sphinx_material
+
 project_dir = os.path.abspath(os.pardir)
 sys.path.insert(0, os.path.join(project_dir, 'src'))
 
@@ -14,7 +16,8 @@ with open(os.path.join(project_dir, 'src', 'thorod', "__about__.py")) as f:
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-	'sphinxarg.ext'
+	'sphinxarg.ext',
+	'sphinx_material',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -77,18 +80,26 @@ intersphinx_mapping = {
 
 # -- Options for HTML output ----------------------------------------------
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_material'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-
+#
 html_theme_options = {
-	'github_user': 'thebigmunch',
-	'github_repo': 'thorod',
-	'github_type': 'star',
-	'show_powered_by': False
+	'nav_title': 'thorod',
+	'color_primary': 'blue',
+	'color_accent': 'deep-orange',
+	'repo_url': 'https://github.com/thebigmunch/thorod',
+	'repo_name': 'thorod',
+	'globaltoc_includehidden': True,
+	'master_doc': False,
 }
+
+# Get the them path
+html_theme_path = sphinx_material.html_theme_path()
+# Register the required helpers for the html context
+html_context = sphinx_material.get_html_context()
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -103,10 +114,8 @@ html_show_sphinx = False
 
 html_sidebars = {
 	'**': [
-		'about.html',
+		'globaltoc.html',
 		'localtoc.html',
-		'relations.html',
 		'searchbox.html',
-		'donate.html',
 	]
 }
